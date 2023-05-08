0bec537a850dc1df31aa59dc822de0f4fcafa8c8
Zaitun Muslimah <zaitunmuslimah34@gmail.com>
2023-05-08 16:26:05 +0700

    baru diubah

---
+++ resources/views/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.global')
+
+@section('title') Create Category @endsection 
+
+@section('content')
+
+TODO: Form create category
+
+<form 
+    enctype="multipart/form-data" 
+    class="bg-white shadow-sm p-3" 
+    action="{{route('categories.store')}}" 
+    method="POST">
+
+    @csrf
+
+</form>
+
+@endsection
