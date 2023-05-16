662dd096cadd22ffd9a96e7b235d1e018583deaa
Zaitun Muslimah <zaitunmuslimah34@gmail.com>
2023-05-16 09:03:00 +0700

    sudah diubah

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Gate;
+
 class BookController extends Controller
 {
+
+    public function __construct(){
+        // OTORISASI GATE
+        $this->middleware(function($request, $next){
+
+          if(Gate::allows('manage-books')) return $next($request);
+        
+          abort(403, 'Anda tidak memiliki cukup hak akses');
+        });
+    }
     /**
      * Display a listing of the resource.
      *
@@ -42,6 +54,17 @@ class BookController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request){
+
+      \Validator::make($request->all(), [
+        "title" => "required|min:5|max:200",
+        "description" => "required|min:20|max:1000",
+        "author" => "required|min:3|max:100",
+        "publisher" => "required|min:3|max:200",
+        "price" => "required|digits_between:0,10",
+        "stock" => "required|digits_between:0,10",
+        "cover" => "required"
+    ])->validate();  
+
         $new_book = new \App\Models\Book;
         $new_book->title = $request->get('title');
         $new_book->description = $request->get('description');
@@ -111,6 +134,22 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
+
+      $book = \App\Models\Book::findOrFail($id);
+
+      \Validator::make($request->all(), [
+          "title" => "required|min:5|max:200",
+          "slug" => [
+              "required",
+              Rule::unique("books")->ignore($book->slug, "slug")
+          ],
+          "description" => "required|min:20|max:1000",
+          "author" => "required|min:3|max:100",
+          "publisher" => "required|min:3|max:200",
+          "price" => "required|digits_between:0,10",
+          "stock" => "required|digits_between:0,10",
+      ])->validate();  
+
         $book = \App\Models\Book::findOrFail($id);
     
         $book->title = $request->get('title');
